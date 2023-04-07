4fad0b9cf62fb08fcb5f2a0c313e77149488bada
Violet-Codes <125933964+Violet-Codes@users.noreply.github.com>
2023-04-07 19:35:13 +0100

    added async support

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,3 +1,5 @@
+use std::{future::Future, pin::Pin};
+
 use super::BFRaw;
 
 pub struct BFCtx<
@@ -67,4 +69,82 @@ pub fn run_bfraw<
     -> ()
 {
     is.iter().for_each(|i| run_bfraw_instruction(ctx, i))
+}
+
+
+pub struct AsyncBFCtx<
+    AskFuture: Future::<Output=u8>,
+    Ask: FnMut() -> AskFuture,
+    Put: FnMut(u8) -> (),
+    Get: FnMut(i32) -> u8,
+    Set: FnMut(i32, u8) -> (),
+    Clear: Fn() -> ()
+>{
+    pub index: i32,
+    pub get: Get,
+    pub set: Set,
+    pub ask: Ask,
+    pub put: Put,
+    pub clear: Clear
+}
+
+pub fn async_run_bfraw_instruction<
+    'a,
+    AskFuture: Future::<Output=u8>,
+    Ask: FnMut() -> AskFuture,
+    Put: FnMut(u8) -> (),
+    Get: FnMut(i32) -> u8,
+    Set: FnMut(i32, u8) -> (),
+    Clear: Fn() -> ()
+>(
+    ctx: &'a mut AsyncBFCtx<AskFuture, Ask, Put, Get, Set, Clear>,
+    i: &'a BFRaw
+)
+    -> Pin<Box<dyn Future<Output=()> + 'a>>
+{
+    Box::pin( async move {
+        match i {
+            BFRaw::Lft => ctx.index -= 1,
+            BFRaw::Rgh => ctx.index += 1,
+            BFRaw::Inc => {
+                let mem = (ctx.get)(ctx.index);
+                if mem == 255 {
+                    (ctx.set)(ctx.index, 0)
+                } else {
+                    (ctx.set)(ctx.index, mem + 1)
+                }
+            },
+            BFRaw::Dec => {
+                let mem = (ctx.get)(ctx.index);
+                if mem == 0 {
+                    (ctx.set)(ctx.index, 255)
+                } else {
+                    (ctx.set)(ctx.index, mem - 1)
+                }
+            },
+            BFRaw::Ask => { (ctx.set)(ctx.index, (ctx.ask)().await); },
+            BFRaw::Put => (ctx.put)((ctx.get)(ctx.index)),
+            BFRaw::Loop(is) => while (ctx.get)(ctx.index) != 0 {
+                async_run_bfraw(ctx, is).await
+            },
+        }
+    })
+}
+
+pub async fn async_run_bfraw<
+    AskFuture: Future::<Output=u8>,
+    Ask: FnMut() -> AskFuture,
+    Put: FnMut(u8) -> (),
+    Get: FnMut(i32) -> u8,
+    Set: FnMut(i32, u8) -> (),
+    Clear: Fn() -> ()
+>(
+    ctx: &mut AsyncBFCtx<AskFuture, Ask, Put, Get, Set, Clear>,
+    is: & Vec<BFRaw>
+)
+    -> ()
+{
+    for i in is {
+        async_run_bfraw_instruction(ctx, i).await
+    }
 }
--- src/repl.rs
+++ src/repl.rs
@@ -1,3 +1,5 @@
+use std::future::Future;
+
 use nibbler::errors::show_error;
 use crate::{ parser::*, interpreter::* };
 
@@ -49,6 +51,63 @@ pub fn rep<
     } else {
         match parse_program()(&mut iter) {
             Ok(is) => run_bfraw(ctx, & is),
+            Err(err) => (console_interactor.write_errln)(format!("{}\n...whilst parsing input", show_error("".to_string(), & show_info, err))),
+        };
+    };
+    true
+}
+
+pub struct AsyncConsoleInteractor<
+    ReadLnFuture: Future::<Output=String>,
+    ReadLn: FnMut(String) -> ReadLnFuture,
+    WriteLn: FnMut(String) -> (),
+    WriteErrLn: FnMut(String) -> (),
+    DisplayHelp: FnMut() -> ()
+>{
+    pub readln: ReadLn,
+    pub writeln: WriteLn,
+    pub write_errln: WriteErrLn,
+    pub display_help: DisplayHelp
+}
+
+pub async fn async_rep<
+    ReadLnFuture: Future::<Output=String>,
+    ReadLn: FnMut(String) -> ReadLnFuture,
+    WriteLn: FnMut(String) -> (),
+    WriteErrLn: FnMut(String) -> (),
+    DisplayHelp: FnMut() -> (),
+    AskFuture: Future::<Output=u8>,
+    Ask: FnMut() -> AskFuture,
+    Put: FnMut(u8) -> (),
+    Get: FnMut(i32) -> u8,
+    Set: FnMut(i32, u8) -> (),
+    Clear: Fn() -> ()
+>(
+    console_interactor: &mut AsyncConsoleInteractor<ReadLnFuture, ReadLn, WriteLn, WriteErrLn, DisplayHelp>,
+    ctx: &mut AsyncBFCtx<AskFuture, Ask, Put, Get, Set, Clear>
+)
+    -> bool
+{
+    let show_info = |info: TextInfo| format!("at {}:{}", info.line, info.index);
+    let s = (console_interactor.readln)("$ ".to_string()).await;
+    let mut iter = TextIter{
+        iter: s.chars(),
+        line: 0,
+        index: 0
+    };
+    if s.chars().nth(0) == Some(':') {
+        match parse_bfcmd()(&mut iter) {
+            Ok(BFCMD::Exit) => return false,
+            Ok(BFCMD::Read(x)) => (console_interactor.writeln)(format!("#{x}: {}", (ctx.get)(x))),
+            Ok(BFCMD::Clear) => (ctx.clear)(),
+            Ok(BFCMD::Help) => (console_interactor.display_help)(),
+            Ok(BFCMD::Find) => (console_interactor.writeln)(format!("head: #{}", ctx.index)),
+            Ok(BFCMD::Move(x)) => ctx.index = x,
+            Err(err) => (console_interactor.write_errln)(format!("{}\n...whilst parsing instruction", show_error("".to_string(), & show_info, err))),
+        };
+    } else {
+        match parse_program()(&mut iter) {
+            Ok(is) => async_run_bfraw(ctx, & is).await,
             Err(err) => (console_interactor.write_errln)(format!("{}\n...whilst parsing input", show_error("".to_string(), & show_info, err))),
         };
     };
